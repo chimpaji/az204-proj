84b5b2476495e7da450aeab42806a526fbe1900c
chimpaji <chimpaji@outlook.com>
2024-03-08 02:33:56 +0000

    update

--- weater-tracker(az-compute)/front/src/App.tsx
+++ weater-tracker(az-compute)/front/src/App.tsx
@@ -5,22 +5,39 @@ import { useEffect, useState } from 'react';
 const azFuncUrl =
   'https://getweathertesttest1212.azurewebsites.net/api/get-weather';
 
+const azSubUrl =
+  'https://getweathertesttest1212.azurewebsites.net/api/subscribe';
+
 function App() {
   const [name, setName] = useState('London');
+  const [email, setEmail] = useState('');
   const [greeting, setGreeting] = useState<string | null>(null);
   const isDev = process.env.NODE_ENV === 'development';
-  const url = isDev ? '/api/get-weather' : azFuncUrl;
-
+  const getWeatherUrl = isDev ? '/api/get-weather' : azFuncUrl;
+  const subscribeUrl = isDev ? '/api/subscribe' : azSubUrl;
   useEffect(() => {
     fetchData();
   }, []);
 
+  console.log({ env: process.env.NODE_ENV });
+
   const fetchData = async (name?: string) => {
     const query = name ? `?name=${name}` : '';
-    const response = await fetch(`${url}${query}`);
+    const response = await fetch(`${getWeatherUrl}${query}`);
     const data = await response.json();
     setGreeting(data);
     console.log({ data });
+  };
+
+  // create subscribeWeather function, save email and city to body, and send to subscribeUrl (post)
+  const subscribeWeather = async (email: string, city: string) => {
+    await fetch(subscribeUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, city }),
+    });
   };
 
   return (
@@ -51,6 +68,27 @@ function App() {
           }}
         >
           Set city
+        </button>
+      </div>
+
+      {/* create a section for subscribe to weather get email and city name */}
+      <div>
+        <h2>Subscribe to weather</h2>
+        <input
+          type='text'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder='Email'
+        />
+
+        <button
+          onClick={async () => {
+            await subscribeWeather(email, name);
+            setEmail('');
+            alert('Subscribed!');
+          }}
+        >
+          Subscribe
         </button>
       </div>
     </>
