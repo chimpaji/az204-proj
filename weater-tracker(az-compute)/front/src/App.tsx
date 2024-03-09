8d7387de6dc06de6ac82ab3e15c53f67cebf4c6b
chimpaji <chimpaji@outlook.com>
2024-03-09 21:57:00 +0000

    update

--- weater-tracker(az-compute)/front/src/App.tsx
+++ weater-tracker(az-compute)/front/src/App.tsx
@@ -26,33 +26,31 @@ function WeatherSubscription() {
   const [subscriptionStatus, setSubscriptionStatus] = useState(null);
 
   const getWeather = () => {
-    fetch(`api/get-weather?name=${cityName}`)
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        setWeatherData(data);
-      })
-      .catch((error) => console.error('Error:', error));
+    // fetch(`api/get-weather?name=${cityName}`)
+    //   .then((response) => {
+    //     return response.json();
+    //   })
+    //   .then((data) => {
+    //     setWeatherData(data);
+    //   })
+    //   .catch((error) => console.error('Error:', error));
   };
 
   const subscribe = () => {
-    fetch('api/subscribe', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ city: cityName, email: email }),
-    })
-      .then((response) => response.json())
-      .then((data) => setSubscriptionStatus(data.body))
-      .catch((error) => console.error('Error:', error));
+    // fetch('api/subscribe', {
+    //   method: 'POST',
+    //   headers: { 'Content-Type': 'application/json' },
+    //   body: JSON.stringify({ city: cityName, email: email }),
+    // })
+    //   .then((response) => response.json())
+    //   .then((data) => setSubscriptionStatus(data.body))
+    //   .catch((error) => console.error('Error:', error));
   };
 
   //create use effect to getWeather of London when mounted
   useEffect(() => {
     getWeather();
   }, []);
-
-  console.log('weatherData', weatherData);
 
   return (
     <div>
