61ca9aa32c58d27b4f6dcccb0ac7b4b18a379f98
chimpaji <chimpaji@outlook.com>
2024-03-09 21:50:14 +0000

    update

--- weater-tracker(az-compute)/front/src/App.tsx
+++ weater-tracker(az-compute)/front/src/App.tsx
@@ -1,36 +1,77 @@
 import './App.css';
 
-import reactLogo from './assets/react.svg';
-import { useState } from 'react';
-import viteLogo from '/vite.svg';
+import { useEffect, useState } from 'react';
+
+import appInsights from 'applicationinsights';
 
 function App() {
-  const [count, setCount] = useState(0);
+  appInsights
+    .setup(process.env.VITE_INST_KEY)
+    .setAutoCollectRequests(true)
+    .start();
 
   return (
-    <>
-      <div>
-        <a href='https://vitejs.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on thse Vite and React logos to learn more
-      </p>
-    </>
+    <div className='App'>
+      <WeatherSubscription />
+    </div>
   );
 }
 
 export default App;
+
+function WeatherSubscription() {
+  const [cityName, setCityName] = useState('London');
+  const [email, setEmail] = useState('');
+  const [weatherData, setWeatherData] = useState(null);
+  const [subscriptionStatus, setSubscriptionStatus] = useState(null);
+
+  const getWeather = () => {
+    fetch(`api/get-weather?name=${cityName}`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setWeatherData(data);
+      })
+      .catch((error) => console.error('Error:', error));
+  };
+
+  const subscribe = () => {
+    fetch('api/subscribe', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ city: cityName, email: email }),
+    })
+      .then((response) => response.json())
+      .then((data) => setSubscriptionStatus(data.body))
+      .catch((error) => console.error('Error:', error));
+  };
+
+  //create use effect to getWeather of London when mounted
+  useEffect(() => {
+    getWeather();
+  }, []);
+
+  console.log('weatherData', weatherData);
+
+  return (
+    <div>
+      <input
+        type='text'
+        value={cityName}
+        onChange={(e) => setCityName(e.target.value)}
+        placeholder='Enter city name'
+      />
+      <input
+        type='email'
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder='Enter email'
+      />
+      <button onClick={getWeather}>Get Weather</button>
+      <button onClick={subscribe}>Subscribe</button>
+      {weatherData && <p>Weather Data: {JSON.stringify(weatherData)}</p>}
+      {subscriptionStatus && <p>Subscription Status: {subscriptionStatus}</p>}
+    </div>
+  );
+}
