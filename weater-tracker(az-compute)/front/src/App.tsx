017e05a0f9bed9aa79a2109fcb7a50461e560c56
chimpaji <chimpaji@outlook.com>
2024-03-09 22:13:36 +0000

    update

--- weater-tracker(az-compute)/front/src/App.tsx
+++ weater-tracker(az-compute)/front/src/App.tsx
@@ -1,6 +1,15 @@
 import './App.css';
 
+import { useEffect, useState } from 'react';
+
+import appInsights from 'applicationinsights';
+
 function App() {
+  appInsights
+    .setup(process.env.VITE_INST_KEY)
+    .setAutoCollectRequests(true)
+    .start();
+
   return <div className='App'>hello world</div>;
 }
 
