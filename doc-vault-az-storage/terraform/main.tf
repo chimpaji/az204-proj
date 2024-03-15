55dee9e2c9b44d3623a3ec3d38229421d808180e
chimpaji <chimpaji@outlook.com>
2024-03-15 02:00:56 +0000

    update

--- doc-vault-az-storage/terraform/main.tf
+++ doc-vault-az-storage/terraform/main.tf
@@ -17,45 +17,61 @@ resource "azurerm_resource_group" "rg" {
   location = local.location
 }
 
-resource "azurerm_service_plan" "sp" {
-  name                = "sp"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  os_type             = "Windows"
-  sku_name            = "F1"
+# resource "azurerm_service_plan" "sp" {
+#   name                = "sp"
+#   resource_group_name = azurerm_resource_group.rg.name
+#   location            = azurerm_resource_group.rg.location
+#   os_type             = "Windows"
+#   sku_name            = "F1"
 
-  depends_on = [azurerm_resource_group.rg]
-}
+#   depends_on = [azurerm_resource_group.rg]
+# }
 
 # create azure web service for node.js(nextjs)
-resource "azurerm_windows_web_app" "web" {
-  name                = "web-8d3ebbb5eba8"
+# resource "azurerm_windows_web_app" "web" {
+#   name                = "web-8d3ebbb5eba8"
+#   resource_group_name = azurerm_resource_group.rg.name
+#   location            = azurerm_service_plan.sp.location
+#   service_plan_id     = azurerm_service_plan.sp.id
+
+#   site_config {
+#     application_stack {
+#       current_stack = "node"
+#       node_version  = "~18"
+#     }
+#     always_on = false
+
+#     cors {
+#       allowed_origins = ["*"]
+#     }
+#   }
+
+#   depends_on = [azurerm_service_plan.sp]
+# }
+
+# # connect to github
+
+# resource "azurerm_app_service_source_control" "example" {
+#   app_id   = azurerm_windows_web_app.web.id
+#   repo_url = "https://github.com/vercel/next.js/tree/canary/examples/hello-world"
+#   branch   = "canary"
+
+#   depends_on = [azurerm_windows_web_app.web]
+
+# }
+
+
+# create azure static web app
+resource "azurerm_static_web_app" "web" {
+  name                = "static-site-8d3ebbb5eba8"
   resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_service_plan.sp.location
-  service_plan_id     = azurerm_service_plan.sp.id
-
-  site_config {
-    application_stack {
-      current_stack = "node"
-      node_version  = "~18"
-    }
-    always_on = false
-
-    cors {
-      allowed_origins = ["*"]
-    }
-  }
-
-  depends_on = [azurerm_service_plan.sp]
+  location            = "westeurope"
+  sku_tier            = "Free"
+  depends_on          = [azurerm_resource_group.rg]
 }
 
-# connect to github
+# output deployment token of the static web app
 
-resource "azurerm_app_service_source_control" "example" {
-  app_id   = azurerm_windows_web_app.web.id
-  repo_url = "https://github.com/vercel/next.js/tree/canary/examples/hello-world"
-  branch   = "canary"
-
-  depends_on = [azurerm_windows_web_app.web]
-
+output "deployment_token" {
+  value = nonsensitive(azurerm_static_web_app.web.api_key)
 }
