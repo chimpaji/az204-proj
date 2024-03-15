e0e30581b1d89aa1653fd925393a5d86e60dba25
chimpaji <chimpaji@outlook.com>
2024-03-15 01:06:25 +0000

    update

--- doc-vault(az-storage)/web-app/src/app/page.tsx
+++ doc-vault(az-storage)/web-app/src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
             target='_blank'
             rel='noopener noreferrer'
           >
-            Byxxfsssx{' '}
+            Byxxfssssx{' '}
             <Image
               src='/vercel.svg'
               alt='Vercel Logo'
