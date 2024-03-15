4e956481a0fa199f4738ed1e2bf66e5c198abf84
chimpaji <chimpaji@outlook.com>
2024-03-15 02:22:10 +0000

    update

--- doc-vault-az-storage/web-app/src/app/page.tsx
+++ doc-vault-az-storage/web-app/src/app/page.tsx
@@ -1,6 +1,8 @@
 import Image from 'next/image';
+import { unstable_noStore as noStore } from 'next/cache';
 
 export default function Home() {
+  noStore();
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <div className='z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex'>
